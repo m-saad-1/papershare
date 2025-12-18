bbcc55b0d5668aefcf5d4c6511b9dc22b8fc7710
msaad23305@gmail.com <msaad23305@gmail.com>
2025-12-18 13:06:14 +0500

    feat: Update database connection settings in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,16 +5,10 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-<<<<<<< HEAD
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s
-      family: 4 // Force IPv4 for broad compatibility
-=======
-      serverSelectionTimeoutMS: 5002, // Timeout after 5s
-      useUnifiedTopology: true,
-      Family: 4
->>>>>>> bb070d24c55c951e2f7f359ffbfe6135b7e0627c
-    });
+const conn = await mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000, // Timeout after 5s
+  family: 4 // Force IPv4 for broad compatibility
+});
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
